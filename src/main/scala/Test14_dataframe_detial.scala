f15447af8ad4123ad8e2d178d6121bc19bc4033b
zhangyingchengqi <1069595532@qq.com>
2020-05-30 17:07:36 +0800

    spark sql中DataFrame常见函数汇总

--- src/main/scala/Test14_dataframe_detial.scala
+++ src/main/scala/Test14_dataframe_detial.scala
@@ -25,6 +25,7 @@ object Test14_dataframe_detial {
     //1. 将dataFrame注册为临时表，后续用sql进行查询
     dataFrame.registerTempTable("students")
 
+    // API方案
     //1. head( num)
     dataFrame.head(3).foreach(println)
     //2. show( num )
@@ -38,10 +39,11 @@ object Test14_dataframe_detial {
     //3. printSchema
     dataFrame.printSchema()
     //4. select
-    dataFrame.select("name", "age").show()
-    //5. withColumn
+    dataFrame.select("name", "age").show()    //直接取列
+    dataFrame.select($"name"+"_", $"age"+1).show()   //加入运算
+    //5. withColumn  -> 添加新列
     dataFrame.withColumn("ageAfter10", dataFrame("age") + 10).show()
-    //6. withColumnRenamed(existing, new)
+    //6. withColumnRenamed(existing, new)   ->重命名,且返回新的DataFrame
     val newDataFrame = dataFrame.withColumnRenamed("age", "ageafter10")
     newDataFrame.show()
 
@@ -54,14 +56,17 @@ object Test14_dataframe_detial {
 
     //排序
     //1. orderBy( cols* )
-    newDataFrame.orderBy(newDataFrame("ageafter10")).show(5)
+    println( "排序" )
+    newDataFrame.orderBy(   newDataFrame("ageafter10")   ).show(5)
     newDataFrame.orderBy("ageafter10", "id").show(5)
-    newDataFrame.sort($"ageafter10".desc).show()
+    newDataFrame.sort(    $"ageafter10".desc   ).show()
 
     //转换
-    newDataFrame.toJSON.collect()
+    println("转换操作")
+    newDataFrame.toJSON.collect().foreach(  println)
     //2. repartition
-    newDataFrame.repartition(4).rdd.partitions.size
+    println("将数据的分区数改为4")
+    println(  newDataFrame.repartition(4).rdd.partitions.size  )
 
     spark.stop()
   }
